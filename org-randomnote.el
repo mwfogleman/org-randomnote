8a7c8fb66dafbd3a25336e64d3dc54f5a3edd95e
Michael Fogleman <michaelwfogleman@gmail.com>
2017-11-14 19:45:17 -0500

    Initial code import.

---
+++ org-randomnote.el
@@ -0,0 +1,73 @@
+;;; org-randomnote.el --- Find a random note in your Org-Mode files
+
+;; Copyright (C) 2017 Michael Fogleman
+
+;; Author: Michael Fogleman <michaelwfogleman@gmail.com>
+;; URL: http://github.com/mwfogleman/org-randomnote
+;; Package-Requires: ((f "0.19.0"))
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; This package implements the "Random Note" functionality popularized
+;; by Tiago Forte with Evernote in Emacs Org-Mode.
+
+;;; Code:
+
+(defvar org-randomnote-candidates org-agenda-files)
+
+;; depends on f.el
+;; https://github.com/rejeep/f.el
+
+;; TODO: Check if selected candidate is empty
+
+(defun file-empty-p (f)
+  (= (f-size f) 0))
+
+(defun org-go-to-header ()
+  (when (equal major-mode 'org-mode)
+    (if (org-at-heading-p)
+	(org-beginning-of-line)
+      (org-up-element))))
+
+(defun org-get-random-file ()
+  (let* ((cnt (length org-randomnote-list))
+	 (nmbr (random cnt)))
+    (nth nmbr org-randomnote-candidates)))
+
+(defun org-get-random-file ()
+  (let* ((cnt (length org-randomnote-list))
+	 (nmbr (random cnt)))
+    (nth nmbr org-randomnote-candidates)))
+
+(defun org-go-to-random-header (f)
+  (find-file f)
+  (let* ((l (random (count-lines (point-min) (point-max)))))
+    (goto-line l)
+    (org-go-to-header)))
+
+(defun org-randomnote ()
+  (interactive)
+  (org-go-to-random-header (org-get-random-file)))
+
+(provide 'org-randomnote)
+
+;;; org-randomnote.el ends here
